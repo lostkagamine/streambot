b3aaf507fbe1b6c58ea438b20430a06ac1f4e31c
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-07-07 17:34:29 +0100

    remove some pointless events

--- events/ban.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    events: ['ban'],
-    code: ctx => {
-        ctx.send(`${ctx[1]} has been banned. Reason: ${ctx[2] || 'none'}.`)
-    }
-}
--- events/timeout.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    events: ['timeout'],
-    code: ctx => {
-        ctx.send(`${ctx[1]} has been timed out for ${ctx[3]} seconds. Reason: ${ctx[2] || 'none'}.`)
-    }
-}
