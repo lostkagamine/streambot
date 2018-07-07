464d675ab55f73a23f189cae55faff8ee5d3cec8
ry00001 <15631667+ry00001@users.noreply.github.com>
2018-07-07 17:17:46 +0100

    fix the thing

---
+++ cmds/about.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'about',
+    code: ctx => {
+        ctx.send('I\'m an instance of StreamBot, a bot made by that one girl, ry00001. I can help moderate your Twitch channel and do other fun things.')
+    }
+}
